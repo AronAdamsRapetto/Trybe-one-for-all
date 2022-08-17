7f632c0fbf948d8a262ff9620579a9e1337e048a
Aron Adams <aronrapetto@hotmail.com>
2022-08-17 01:46:19 -0300

    desafio 4 concluido

---
+++ desafio4.sql
@@ -0,0 +1,4 @@
+SELECT us.usuario, IF(MAX(hi_us.data_reproducao) <= '2021', 'Usuário inativo', 'Usuário ativo') AS `status_usuario` FROM SpotifyClone.usuarios AS us
+JOIN SpotifyClone.historico_usuario AS hi_us ON hi_us.usuario_id = us.usuario_id
+GROUP BY hi_us.usuario_id
+ORDER BY us.usuario ASC;
