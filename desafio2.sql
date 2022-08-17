1add7ca315864c574dc5b018054db7a6f84fc98c
Aron Adams <aronrapetto@hotmail.com>
2022-08-17 02:11:33 -0300

    desafios 2 e 4 corrigidos

--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(DISTINCT ca.cancao) AS cancoes, COUNT(DISTINCT ar.artista) AS artistas, COUNT(DISTINCT al.album) AS albuns FROM SpotifyClone.cancoes AS ca
-JOIN SpotifyClone.albuns AS al ON ca.album_id = al.album_id
-JOIN SpotifyClone.artistas AS ar ON al.artista_id = ar.artista_id;
+SELECT COUNT(ca.cancao_id) AS `cancoes`, COUNT(DISTINCT ar.artista_id) AS `artistas`, COUNT(DISTINCT al.album_id) AS `albuns` FROM SpotifyClone.cancoes AS ca
+RIGHT JOIN SpotifyClone.albuns AS al ON ca.album_id = al.album_id
+RIGHT JOIN SpotifyClone.artistas AS ar ON al.artista_id = ar.artista_id;
--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT us.usuario, IF(MAX(hi_us.data_reproducao) <= '2021', 'Usuário inativo', 'Usuário ativo') AS `status_usuario` FROM SpotifyClone.usuarios AS us
+SELECT us.usuario, IF(MAX(YEAR(hi_us.data_reproducao)) < '2021', 'Usuário inativo', 'Usuário ativo') AS `status_usuario` FROM SpotifyClone.usuarios AS us
 JOIN SpotifyClone.historico_usuario AS hi_us ON hi_us.usuario_id = us.usuario_id
 GROUP BY hi_us.usuario_id
 ORDER BY us.usuario ASC;
